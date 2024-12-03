5a8594a6a931e03788b993fcadbd8925ccaead65
Abbas-Dev-786 <abbasbhanpura.dev@gmail.com>
2024-12-03 21:58:21 +0530

    feat: made sidebar responsive

--- src/components/shared/header-breadcrumbs.tsx
+++ src/components/shared/header-breadcrumbs.tsx
@@ -10,28 +10,32 @@ import {
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 import Link from "next/link";
+import { SidebarTrigger } from "../ui/sidebar";
 
 const HeaderBreadcrumbs = () => {
   const pathName = usePathname();
   const paths = pathName === "/" ? [""] : pathName.split("/");
 
   return (
-    <Breadcrumb>
-      <BreadcrumbList>
-        {paths.map((path, i) => (
-          <React.Fragment key={`path-${i}`}>
-            <BreadcrumbItem>
-              <BreadcrumbLink asChild>
-                <Link href={path} className="capitalize">
-                  {path === "" ? "home" : path.split("-").join(" ")}
-                </Link>
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            {paths.length - 1 != i && <BreadcrumbSeparator />}
-          </React.Fragment>
-        ))}
-      </BreadcrumbList>
-    </Breadcrumb>
+    <div className="flex items-center justify-start">
+      <SidebarTrigger className="inline md:hidden" />
+      <Breadcrumb>
+        <BreadcrumbList>
+          {paths.map((path, i) => (
+            <React.Fragment key={`path-${i}`}>
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <Link href={path} className="capitalize">
+                    {path === "" ? "home" : path.split("-").join(" ")}
+                  </Link>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              {paths.length - 1 != i && <BreadcrumbSeparator />}
+            </React.Fragment>
+          ))}
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
   );
 };
 
